e0db9d23d058e23989629ebd0479dc588390e086
Stephan Lanberg <stephan.lanberg@stellarforce.net>
2023-05-31 10:47:01 +0200

    Small twig

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -218,7 +218,7 @@ class _HomePageState extends State<HomePage> {
               expand: false,
               initialChildSize: 0.75,
               maxChildSize: 0.75,
-              minChildSize: 0.70,
+              minChildSize: 0.30,
               builder: (context, scrollController) => SingleChildScrollView(
                 controller: scrollController,
                 child: Column(
